cc7dfc831f04c394a80e58fb95c52b8d4b69ef13
Raduc2003 <72871085+Raduc2003@users.noreply.github.com>
2023-05-13 19:28:36 +0300

    signup

--- routes/signup.js
+++ routes/signup.js
@@ -4,4 +4,8 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.render("signup");
 });
+router.post("/", (req, res) => {
+  console.log(req.body.username);
+  res.send("asda");
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 app.use(express.static("Public"));
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
 app.get("/", (req, res) => {
   res.render("index");
 });
